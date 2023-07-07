ae8edbd848e2715a50b3005b00baa9ea6df61dcc
ziad0ayman <zezokmezo@gmail.com>
2023-07-07 13:29:34 -0700

    add 6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int haha(int n, int i);
+int prime(int n, int a);
+
+/**
+ * is_prime_number - checks for prime numbers
+ * @n: the number
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime(n, n - 1));
+}
+
+/**
+ * prime - the recursive function
+ * @n: the number to check
+ * @a: counter
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int prime(int n, int a)
+{
+	if (n % a == 0)
+		return (0);
+	prime(n, a - 1);
+	if (a == 1)
+		return (1);
+}
